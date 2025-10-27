4caf64d103df5d72cd51ff8b5640ea8f1d87ede8
TamHocDev <tamtrandinh3092004@gmail.com>
2025-10-27 22:20:31 +0700

    hotel-booking

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar'
 import { Route, useLocation, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
+import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
 
 const App = () => {
 
@@ -10,14 +14,18 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-      <div className='min-h-[70vh]'>
+      {false &&<HotelReg />}
+      <div className="min-h-[70vh]">
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App
